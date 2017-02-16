2844c11e11e93517e2d9acbad272eb7eb2b96644
Franz Noel Tanglao <franz@asteriainteractive.com>
2017-02-16 00:15:59 -0800

    version: Added version 1-6 of asteriainteractive.com

---
+++ admin.py
@@ -0,0 +1,82 @@
+from google.appengine.api import mail
+import webapp2, jinja2, os, sys, model
+
+
+environment = jinja2.Environment( 
+  loader = jinja2.FileSystemLoader( os.path.join( os.path.dirname(__file__),'templates','admin') ), 
+  extensions=['jinja2.ext.autoescape'] 
+)
+
+class BaseHandler(webapp2.RequestHandler):
+  def render_html(self,temp,val=''):
+    self.response.content_type = 'text/html'
+    try:
+      t = environment.get_template(temp)
+      self.response.out.write(t.render(val))
+    except jinja2.TemplateNotFound:
+      t = environment.get_template('error_default.html')
+      self.response.out.write(t.render(val))
+      
+class AdminMainHandler(BaseHandler):
+  def get(self):
+    val = {'greeting':'Hello World!'}
+    self.render_html('index.html',val)
+    
+# class ContactFormHandler(BaseHandler):
+#   def post(self):
+#     # Save to Contact
+#     contact = model.Contact()
+#     contact.fullname = self.request.get("fullname")
+#     contact.email = self.request.get("email")
+#     contact.phoneNumber= self.request.get("phoneNumber")
+#     contact.website = self.request.get("website")
+#     contact.subject = self.request.get("subject")
+#     contact.message = self.request.get("message")
+#     contact.put()
+    
+#     # Email
+#     message = mail.EmailMessage(
+#       sender = self.request.get("fullname") + "<" + self.request.get("email") + ">",
+#       subject= "AI Contact: "+self.request.get("subject")
+#     )
+#     message.to = "Franz Noel Tanglao <franz@asteriainteractive.com>"
+#     message.body = self.request.get("message")
+#     message.send()
+#     #self.redirect("/contact")
+    
+    
+# class RegistrationFormHandler(BaseHandler):
+#   def post(self):
+#     customer = model.Customers()
+#     customer.kind = "reseller#customer"
+#     customer.fullname = self.request.get("contactName")
+#     customer.customerDomain = self.request.get("customerDomain")
+#     customer.phoneNumber = self.request.get("phoneNumber")
+#     customer.alternateEmail = self.request.get("alternateEmail")
+#     customer.resourceUiUrl = self.request.get("resourceUiUrl")
+#     customer_key = customer.put()
+#     customerAddress = model.CustomerAddress(parent=customer_key)
+#     customerAddress.kind = "customers#address"
+#     customerAddress.contactName = self.request.get("contactName")
+#     customerAddress.organizationName = self.request.get("organizationName")
+#     customerAddress.locality = self.request.get("locality")
+#     customerAddress.region = self.request.get("region")
+#     customerAddress.postalCode = self.request.get("postalCode")
+#     customerAddress.countryCode = "+1"
+#     customerAddress.addressLine1 = self.request.get("addressLine1")
+#     customerAddress.addressLine2 = self.request.get("addressLine2")
+#     customerAddress.addressLine3 = self.request.get("addressLine3")
+#     customerAddress.put()
+#     self.redirect("/registration")
+
+# class StaticHandler(BaseHandler):
+#   def get(self, url):
+#     self.render_html(url+'.html')
+
+app = webapp2.WSGIApplication([
+    ('/admin/', AdminMainHandler),
+    ('/pages/', AdminMainHandler),
+    # ('/contact/', ContactFormHandler),
+    # ('/registration/', RegistrationFormHandler),
+    # ('/(.*?)', StaticHandler),
+], debug=False)
--- main.py
+++ main.py
@@ -1,132 +1,80 @@
-import webapp2, jinja2, os, re, cgi, hmac, random, string
-from models import Account, Job, Resume
+import webapp2, jinja2, os, sys, model
 from google.appengine.api import mail
-from google.appengine.ext import db
 
-environment = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
-config = {}
-config['webapp2_extras.sessions'] = {
-  'secret_key': '(spg@j&v76ddh)bq6xcs0di&3uevqh(*_7tnt04!r(yw3+pqn5'
-}
+environment = jinja2.Environment( 
+  loader = jinja2.FileSystemLoader( os.path.join( os.path.dirname(__file__),'templates','professional') ), 
+  extensions=['jinja2.ext.autoescape'] 
+)
 
-class IndexHandler(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+  def render_html(self,temp,val=''):
+    self.response.content_type = 'text/html'
+    try:
+      t = environment.get_template(temp)
+      self.response.out.write(t.render(val))
+    except jinja2.TemplateNotFound:
+      t = environment.get_template('error_default.html')
+      self.response.out.write(t.render(val))
+      
+class MainHandler(BaseHandler):
   def get(self):
     val = {'greeting':'Hello World!'}
-    template = environment.get_template('index.html')
-    self.response.out.write(template.render(val))
+    self.render_html('index.html',val)
     
-
-class AccountHandler(webapp2.RequestHandler):
-  def get(self):
-    self.redirect("/")
-    
+class ContactFormHandler(BaseHandler):
   def post(self):
-    ## Get account request information
-    fullname= cgi.escape(self.request.get('fullname'))
-    username= cgi.escape(self.request.get('username'))
-    email = cgi.escape(self.request.get('email'))
-    password = self.hash_password(self.request.get('username'),self.request.get('password'))
-    self.send_confirmation_email(email, fullname)
-    self.save_info(username, fullname, email, password)
-    self.redirect("/")
+    # Save to Contact
+    contact = model.Contact()
+    contact.fullname = self.request.get("fullname")
+    contact.email = self.request.get("email")
+    contact.phoneNumber= self.request.get("phoneNumber")
+    contact.website = self.request.get("website")
+    contact.subject = self.request.get("subject")
+    contact.message = self.request.get("message")
+    contact.put()
     
-  def save_info(self,username,fullname,email,password):
-    account= Account(
-      key_name=username,
-      fullname=fullname,
-      username= username,
-      email = email,
-      password = password,
+    # Email
+    message = mail.EmailMessage(
+      sender = self.request.get("fullname") + "<" + self.request.get("email") + ">",
+      subject= "AI Contact: "+self.request.get("subject")
     )
-    account.put()
-    
-  def send_confirmation_email(self,email,fullname): # Send Email
-    mail.send_mail(
-      sender="Asteria Interactive <franz@asteriainteractive.com>", 
-      to=fullname+" <"+email+">",
-      subject="Confirm Your Asteria Account",
-      body="Hi "+fullname+",\n\nWelcome to Asteria Interactive!\n\nClick on the link below to confirm your email address.\n\nThanks!\nAsteria Interactive"
-    )
-    
-  def hash_password(self,username,password):
-    salt = ''.join(random.sample(string.letters,5))
-    secret = config['webapp2_extras.sessions']['secret_key']
-    hashed = hmac.new(secret,username+password+salt).hexdigest()
-    hashed_password = '%s|%s' % (hashed,salt)
-    return hashed_password
+    message.to = "Franz Noel Tanglao <franz@asteriainteractive.com>"
+    message.body = self.request.get("message")
+    message.send()
+    #self.redirect("/contact")
     
     
-class JobHandler(webapp2.RequestHandler):
-  def get(self):
-    self.redirect("/")
-
+class RegistrationFormHandler(BaseHandler):
   def post(self):
-    skills = [x.strip() for x in self.request.get('skills').split(',')]
-    job = Job(
-      # create the key_name, and parent
-      title = self.request.get('title'),
-      description = self.request.get('description'),
-      address = self.request.get('address'),
-      skillsets = skills,
-      phone = self.request.get('phone'),
-      link = self.request.get('link'),
-      isPublic = self.request.get('isPublic'),
-    )
-    job.put()
-    self.redirect("/")
+    customer = model.Customers()
+    customer.kind = "reseller#customer"
+    customer.fullname = self.request.get("contactName")
+    customer.customerDomain = self.request.get("customerDomain")
+    customer.phoneNumber = self.request.get("phoneNumber")
+    customer.alternateEmail = self.request.get("alternateEmail")
+    customer.resourceUiUrl = self.request.get("resourceUiUrl")
+    customer_key = customer.put()
+    customerAddress = model.CustomerAddress(parent=customer_key)
+    customerAddress.kind = "customers#address"
+    customerAddress.contactName = self.request.get("contactName")
+    customerAddress.organizationName = self.request.get("organizationName")
+    customerAddress.locality = self.request.get("locality")
+    customerAddress.region = self.request.get("region")
+    customerAddress.postalCode = self.request.get("postalCode")
+    customerAddress.countryCode = "+1"
+    customerAddress.addressLine1 = self.request.get("addressLine1")
+    customerAddress.addressLine2 = self.request.get("addressLine2")
+    customerAddress.addressLine3 = self.request.get("addressLine3")
+    customerAddress.put()
+    self.redirect("/registration")
 
+class StaticHandler(BaseHandler):
+  def get(self, url):
+    self.render_html(url+'.html')
 
-class ResumeHandler(webapp2.RequestHandler):
-  def get(self):
-    self.redirect("/")
-    
-  def post(self):
-    skills = [x.strip() for x in self.request.get('skills').split(',')]
-    resume = Resume(
-      # create the key_name, and parent
-      title = self.request.get('title'),
-      description = self.request.get('description'),
-      address = self.request.get('address'),
-      skillsets = skills,
-      link = self.request.get('link'),
-      isPublic = self.request.get('isPublic'),
-    )
-    resume.put()
-    self.redirect('/')
-    
-class LoginHandler(webapp2.RequestHandler):
-  def post(self):
-    username = self.request.get('username')
-    password = self.request.get('password')
-    if self.valid_password(username,password):
-      users = db.GqlQuery("""SELECT * FROM Account WHERE username=:1""",username)
-      for user in users.run():
-        self.response.set_cookie('username',user.fullname,max_age=360,path='/')
-        self.response.set_cookie('message','',max_age=360,path='/')
-    else:
-      self.response.set_cookie('message','Login failed!',max_age=360,path='/')
-    self.redirect('/')
-    
-  def valid_password(self,username,password):
-    users = db.GqlQuery("""SELECT * FROM Account WHERE username=:1""",username)
-    for user in users.run():
-      if user:
-        # get salt, get secret, hash new password, and compare 2 hashed passwords
-        hashed = user.password.split("|")
-        salt = hashed[1]
-        current_hashed_password = hashed[0]
-        secret = config['webapp2_extras.sessions']['secret_key']
-        hashed_password = hmac.new(secret,username+password+salt).hexdigest()
-        if hashed_password==current_hashed_password:
-          return True
-        else:
-          return False
-  
-    
 app = webapp2.WSGIApplication([
-  ('/', IndexHandler),
-  ('/account/', AccountHandler),
-  ('/job/', JobHandler),
-  ('/login/', LoginHandler),
-  ('/resume/', ResumeHandler),
-], debug=True)
+    ('/', MainHandler),
+    ('/contact/', ContactFormHandler),
+    ('/registration/', RegistrationFormHandler),
+    ('/(.*?)', StaticHandler),
+], debug=False)
